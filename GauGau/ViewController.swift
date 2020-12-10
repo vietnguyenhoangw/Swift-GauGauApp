8c00f38597e76bec7a2d170e1a4035120f97c056
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2020-12-10 09:56:45 +0700

    Initial Commit

---
+++ GauGau/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GauGau
+//
+//  Created by coder on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
